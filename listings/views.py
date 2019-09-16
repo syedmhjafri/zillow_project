28d8d3e166b2a35b273b0a5a4e55950620134646
Syed Muhammad Jafri <syedjafri@nyu.edu>
2019-09-16 00:39:22 -0400

    Added new file choices.py

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Listing
 
@@ -17,7 +17,13 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.choices import price_choices, bedroom_choices, state_choices 
 
 from listings.models import Listing
 from realtors.models import Realtor
@@ -9,7 +10,10 @@ from realtors.models import Realtor
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     context = {
-        'listings' : listings
+        'listings' : listings,
+        'state_choices' : state_choices,
+        'price_choices' : bedroom_choices,
+        'price_choices' : price_choices,
     }
     return render(request, 'pages/index.html', context)
 
@@ -22,7 +26,7 @@ def about(request):
     mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
 
     context = {
-        'relators' : realtors,
+        'realtors' : realtors,
         'mvp_realtors' : mvp_realtors
     }
 
