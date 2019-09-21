b42a0b768fede3efd7f2be3ebafefb4b6384c181
Syed Muhammad Jafri <syedjafri@nyu.edu>
2019-09-21 16:11:13 -0400

    final touches added before making the first git commit online. Further improvements will be added later

--- btre/settings.py
+++ btre/settings.py
@@ -146,3 +146,10 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     
 }
+
+# Email configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USER_TLS = True
